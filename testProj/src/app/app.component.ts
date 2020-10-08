418c4e32c2f00e65c3d8ce0e3e7ea91711ee9b91
Vlad <v.rozanoff@gmail.com>
2020-10-08 23:11:50 +0300

    Set image changing after 5 seconds elapse

--- testProj/src/app/app.component.ts
+++ testProj/src/app/app.component.ts
@@ -6,9 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Timeout is over');
+      this.img = 'https://cdn.worldvectorlogo.com/logos/angular-icon.svg';
+      }, 5000);
+  }
   title = 'Dynamic title';
   number = 42;
   arr = [1, 2, 3];
 
   obj = {a: 1, b: 2, c: {d: 2}};
+
+  img = 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg';
 }
