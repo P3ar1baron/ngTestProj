4b1a0ff890f6713cd80a357897d9d32b76a49aef
Vlad <v.rozanoff@gmail.com>
2020-10-09 11:20:34 +0300

    Practice event binding

--- testProj/src/app/app.component.ts
+++ testProj/src/app/app.component.ts
@@ -5,19 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
 
-  constructor() {
-    setTimeout(() => {
-      console.log('Timeout is over');
-      this.img = 'https://cdn.worldvectorlogo.com/logos/angular-icon.svg';
-      }, 5000);
-  }
+  // number = 42;
+  // arr = [1, 2, 3];
+  //
+  // obj = {a: 1, b: 2, c: {d: 2}};
   title = 'Dynamic title';
-  number = 42;
-  arr = [1, 2, 3];
 
-  obj = {a: 1, b: 2, c: {d: 2}};
+  inputValue = '';
 
-  img = 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg';
+  constructor() {
+
+  }
+
+  // tslint:disable-next-line:typedef
+  onInput(event?) {
+    console.log('Event', event);
+  }
 }
