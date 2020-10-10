67d6ab35f78e6c0051528046e758683a4d4bbe67
Vlad <v.rozanoff@gmail.com>
2020-10-10 14:05:41 +0300

    Output object on the view via ngFor directive

--- testProj/src/app/app.component.ts
+++ testProj/src/app/app.component.ts
@@ -9,4 +9,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   arr = [1, 1, 2, 3, 5, 8, 13];
+
+  objs = [
+    {title: 'Post 1', author: 'Vlad', comments: [
+        {name: 'Max', text: 'lorem 1'},
+        {name: 'Max', text: 'lorem 2'},
+        {name: 'Max', text: 'lorem 3'},
+      ]},
+    {title: 'Post 2', author: 'Mirko', comments: [
+        {name: 'Dima', text: 'lorem 1'},
+        {name: 'Dima', text: 'lorem 2'},
+        {name: 'Dima', text: 'lorem 3'},
+      ]}
+  ];
 }
