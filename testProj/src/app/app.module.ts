5b4d417d22bff7e199239a129ff6c472c2561544
Vlad <v.rozanoff@gmail.com>
2020-10-14 13:28:44 +0300

    Remove unnecessary components

--- testProj/src/app/app.module.ts
+++ testProj/src/app/app.module.ts
@@ -3,19 +3,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule} from '@angular/common';
 
 import { AppComponent } from './app.component';
-import {PostComponent} from './post/post.component';
-import { Post2Component } from './post2/post2.component';
-import { Post3Component } from './post3/post3.component';
-import {Post4Component} from './post4/post4.component';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostComponent,
-    Post2Component,
-    Post3Component,
-    Post4Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
