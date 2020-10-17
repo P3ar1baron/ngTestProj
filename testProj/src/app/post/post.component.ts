3c42febc97d8154bfd9f0be07ee4367bbf9a43f7
Vlad <v.rozanoff@gmail.com>
2020-10-17 21:59:33 +0300

    Remove semicolons and edit linter config

--- testProj/src/app/post/post.component.ts
+++ testProj/src/app/post/post.component.ts
@@ -1,23 +1,30 @@
-import {Component, ContentChild, ElementRef, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
-import { Post } from '../app.component';
+import {Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core'
+import { Post } from '../app.component'
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent implements OnInit, OnChanges {
+export class PostComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck
+{
 
-  @Input() post: Post;
-  @ContentChild('info', {static: true}) infoRef: ElementRef;
+  @Input() post: Post
+  @ContentChild('info', {static: true}) infoRef: ElementRef
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('ngOnChanges', changes);
+    console.log('ngOnChanges', changes)
   }
 
   ngOnInit() {
-    console.log('ngOnInit');
+    console.log('ngOnInit')
     // console.log(this.infoRef.nativeElement);
   }
 
+  ngDoCheck() {
+    console.log('ngDoCheck')
+  }
 }
