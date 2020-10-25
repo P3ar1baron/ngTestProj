7e92c0b8af60d90e3779c237641b0b653b65681b
Vlad <v.rozanoff@gmail.com>
2020-10-25 21:41:40 +0200

    Use input parameters for actions in directives Use HostBinding

--- testProj/src/app/directives/style.directive.ts
+++ testProj/src/app/directives/style.directive.ts
@@ -1,10 +1,15 @@
-import {Directive, ElementRef, HostListener, Inject, Renderer2} from '@angular/core'
+import {Directive, ElementRef, HostBinding, HostListener, Inject, Input, Renderer2} from '@angular/core'
 
 @Directive({
   selector: '[appStyle]',
 
 })
 export class StyleDirective {
+  @Input('appStyle') color = 'blue'
+  @Input() dStyles: {border?: string, fontWeight?: string, borderRadius?: string}
+
+  @HostBinding('style.color') elColor = null
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
   }
 
@@ -13,10 +18,18 @@ export class StyleDirective {
   }
 
   @HostListener('mouseenter') onEnter() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'blue')
+    this.elColor = this.color
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color',  this.color)
+    // this.renderer.setStyle(this.elRef.nativeElement, 'fontWeight',  this.dStyles.fontWeight)
+    // this.renderer.setStyle(this.elRef.nativeElement, 'border',  this.dStyles.border)
+    // this.renderer.setStyle(this.elRef.nativeElement, 'borderRadius',  this.dStyles.borderRadius)
   }
 
   @HostListener('mouseleave') onLeave() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', null)
+    this.elColor = null
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', null)
+    // this.renderer.setStyle(this.elRef.nativeElement, 'fontWeight', null)
+    // this.renderer.setStyle(this.elRef.nativeElement, 'border',  null)
+    // this.renderer.setStyle(this.elRef.nativeElement, 'borderRadius',  null)
   }
 }
