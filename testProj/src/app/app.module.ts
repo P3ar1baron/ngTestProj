fe29a8c72c69b64750617a08f8bb347db7e79f4d
Vlad <v.rozanoff@gmail.com>
2020-10-25 12:42:15 +0200

    Remove post and post-form components

--- testProj/src/app/app.module.ts
+++ testProj/src/app/app.module.ts
@@ -4,14 +4,10 @@ import { CommonModule} from '@angular/common';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import { PostFormComponent } from './post-form/post-form.component';
-import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostFormComponent,
-    PostComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
