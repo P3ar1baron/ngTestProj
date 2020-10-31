c4b183d78dcc199f7c0ff85ca649253edd57f39e
Vlad <v.rozanoff@gmail.com>
2020-10-31 23:45:04 +0200

    Practice new pipes

--- testProj/src/app/app.component.ts
+++ testProj/src/app/app.component.ts
@@ -7,5 +7,17 @@ import {Component} from '@angular/core'
 })
 
 export class AppComponent {
-  date:Date = new Date()
+
+  float = 0.42
+
+  obj = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: 3,
+        f: 4
+      }
+    }
+  }
 }
