62ce6c06bd3ede6c40bebeb35d5efcb4b5f484ea
Vlad <v.rozanoff@gmail.com>
2020-11-01 14:37:21 +0200

    Add new pipe

--- testProj/src/app/app.module.ts
+++ testProj/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { CommonModule} from '@angular/common'
 
 import { AppComponent } from './app.component'
 import {FormsModule} from '@angular/forms'
+import {MultByPipe} from "./pipes/mult-by.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultByPipe
   ],
   imports: [
     BrowserModule,
