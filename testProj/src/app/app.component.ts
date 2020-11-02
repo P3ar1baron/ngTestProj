e0da6f7840e52be9cc2117666fc3e3f85fd37664
Vlad <v.rozanoff@gmail.com>
2020-11-02 23:04:56 +0200

    Use classic approach for streams

--- testProj/src/app/app.component.ts
+++ testProj/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 import {Observable} from 'rxjs'
 
 export  interface Post {
@@ -12,7 +12,7 @@ export  interface Post {
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   promise: Promise<string> = new Promise<string>( resolve => {
     setTimeout(() => {
@@ -20,9 +20,17 @@ export class AppComponent {
     }, 4000)
   })
 
-  date: Observable<Date> = new Observable<Date>(obs => {
+  date$: Observable<Date> = new Observable<Date>(obs => {
     setInterval(() => {
       obs.next(new Date())
     }, 1000)
   })
+
+  date: Date
+
+  ngOnInit(): void {
+    this.date$.subscribe(date => {
+      this.date = date
+    })
+  }
 }
