8a3476e86a2ded1162b5adcff9d2386e8a285496
Vlad <v.rozanoff@gmail.com>
2020-11-02 22:46:44 +0200

    Clean app component

--- testProj/src/app/app.component.ts
+++ testProj/src/app/app.component.ts
@@ -13,20 +13,4 @@ export  interface Post {
 
 export class AppComponent {
 
-  posts: Post[] = [
-    {title: 'Beer', text: 'Best beer in the world'},
-    {title: 'Bread', text: 'Best bread in the world'},
-    {title: 'Javascript', text: 'Best language in the world'},
-  ]
-
-  search = ''
-  searchField = 'title'
-
-
-  addPost() {
-    this.posts.unshift({
-      title: 'Angular 8',
-      text: 'Vlad Angular'
-    })
-  }
 }
