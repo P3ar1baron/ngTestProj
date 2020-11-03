30dd6f6bb1069db3bbeef618d95cd1d5d10d7380
Vlad <v.rozanoff@gmail.com>
2020-11-03 18:23:07 +0200

    Add Counter component with injected services

--- testProj/src/app/app.module.ts
+++ testProj/src/app/app.module.ts
@@ -7,14 +7,16 @@ import {FormsModule} from '@angular/forms'
 import {MultByPipe} from './pipes/mult-by.pipe';
 import { ExMarksPipe } from './pipes/ex-marks.pipe';
 import { FilterPipe } from './pipes/filter.pipe'
-import {AppCounterService} from './services/app-counter.service'
+import {AppCounterService} from './services/app-counter.service';
+import { CounterComponent } from './counter/counter.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     MultByPipe,
     ExMarksPipe,
-    FilterPipe
+    FilterPipe,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
