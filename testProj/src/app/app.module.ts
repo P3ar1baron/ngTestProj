92182c3cc9f7e17f4306bab8985938ef7a2c7d56
Vlad <v.rozanoff@gmail.com>
2020-11-08 21:54:26 +0200

    Add AboutPage module and Shared module

--- testProj/src/app/app.module.ts
+++ testProj/src/app/app.module.ts
@@ -4,25 +4,21 @@ import {NgModule} from '@angular/core'
 import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms'
 import {HomePageComponent} from './home-page/home-page.component'
+import {AboutPageModule} from './about-page/about-page.module'
+import {SharedModule} from './shared/shared.module'
 import {AppRoutingModule} from './app-routing.module'
-import {AboutPageComponent} from './about-page/about-page.component'
-import {AboutExtraPageComponent} from './about-page/about-extra-page/about-extra-page.component'
-import {ColorDirective} from './shared/color.directive'
-import {PageNamePipe} from './shared/page-name.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
-    AboutPageComponent,
-    AboutExtraPageComponent,
-    ColorDirective,
-    PageNamePipe
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    AboutPageModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
