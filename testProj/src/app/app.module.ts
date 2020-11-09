02c412a0bcd5330f993391bb758fcb04563f0a7c
Vlad <v.rozanoff@gmail.com>
2020-11-09 23:59:14 +0200

    Clean project for forms practice

--- testProj/src/app/app.module.ts
+++ testProj/src/app/app.module.ts
@@ -3,20 +3,14 @@ import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms'
-import {HomePageComponent} from './home-page/home-page.component'
-import {SharedModule} from './shared/shared.module'
-import {AppRoutingModule} from './app-routing.module'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomePageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
-    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
