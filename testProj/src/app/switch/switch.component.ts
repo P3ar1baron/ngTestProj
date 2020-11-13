4c4b8afbd07ff85bcb733e2b8f03f9f76407e9c3
Vlad <v.rozanoff@gmail.com>
2020-11-13 21:37:13 +0200

    Add appState variable Add VALUE_ACCESSOR

--- testProj/src/app/switch/switch.component.ts
+++ testProj/src/app/switch/switch.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, forwardRef, OnInit, Provider} from '@angular/core'
+import {NG_VALUE_ACCESSOR} from '@angular/forms'
 
+
+const VALUE_ACCESSOR: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef((() => SwitchComponent)),
+  multi: true
+}
 @Component({
   selector: 'app-switch',
   templateUrl: './switch.component.html',
