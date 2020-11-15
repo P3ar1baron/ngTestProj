33c95712219fa1b8a11c2c97904dcec0a3ed8a65
Vlad <v.rozanoff@gmail.com>
2020-11-15 21:47:21 +0200

    Implement get and post for todos Add delay for loading block for user-friendly experience

--- testProj/src/app/app.component.ts
+++ testProj/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core'
 import {HttpClient} from '@angular/common/http'
+import {delay} from "rxjs/operators";
 
 export interface ToDo {
   completed: boolean
@@ -17,16 +18,42 @@ export class AppComponent implements OnInit {
 
   todos: ToDo[] = []
 
+  loading = false
+
+  todoTitle = ''
+
   constructor(private http: HttpClient) {}
 
 
   ngOnInit(): void {
-    this.http.get<ToDo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
-      .subscribe(todos => {
-        console.log('Response', todos)
-        this.todos = todos
-      })
+    this.fetchToDos()
   }
 
 
+  addToDo() {
+    if (!this.todoTitle.trim()) {
+      return
+    }
+
+    const newToDo: ToDo = {
+      title: this.todoTitle,
+      completed: false
+    }
+
+    this.http.post<ToDo>('https://jsonplaceholder.typicode.com/todos', newToDo)
+      .subscribe( todo => {
+        this.todos.push(todo)
+        this.todoTitle = ''
+      })
+  }
+
+  fetchToDos() {
+    this.loading = true
+    this.http.get<ToDo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .pipe(delay(1500))
+      .subscribe(todos => {
+        this.todos = todos
+        this.loading = false
+      })
+  }
 }
