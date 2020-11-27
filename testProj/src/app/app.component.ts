39f03d0998a97dce343461ce053e816afdf87648
Vlad <v.rozanoff@gmail.com>
2020-11-28 00:05:02 +0200

    Add auth guard and service

--- testProj/src/app/app.component.ts
+++ testProj/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core'
+import {AuthService} from './auth.service'
 
 
 @Component({
@@ -6,10 +7,8 @@ import {Component, OnInit} from '@angular/core'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  ngOnInit(): void {
+  constructor(private auth: AuthService) {
   }
-
-
 }
