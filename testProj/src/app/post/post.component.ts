d2edc257318b9c0bbe73b7502d69625f4d0a1825
Vlad <v.rozanoff@gmail.com>
2020-11-28 11:46:22 +0200

    Add PostResolver and register route for it

--- testProj/src/app/post/post.component.ts
+++ testProj/src/app/post/post.component.ts
@@ -13,13 +13,19 @@ export class PostComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router,
-    private  postsService: PostsService) { }
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => {
-      this.post = this.postsService.getById(+params.id)
+    // this.post = this.route.snapshot.data.post
+
+    this.route.data.subscribe(data => {
+      this.post = data.post
     })
+
+    // this.route.params.subscribe((params: Params) => {
+    //   this.post = this.postsService.getById(+params.id)
+    // })
   }
 
 
