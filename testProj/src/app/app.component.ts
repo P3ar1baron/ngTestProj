a40daf84d250ac074b282d8f0c285b3b0eefd7ca
Vlad <v.rozanoff@gmail.com>
2020-12-03 23:15:59 +0200

    Add ref directive and show modal method for creating dynamic modal window instance, similar to Activator.CreateInstanceOf

--- testProj/src/app/app.component.ts
+++ testProj/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, ComponentFactoryResolver, OnInit, ViewChild} from '@angular/core'
+import {ModalComponent} from "./modal/modal.component";
+import {RefDirective} from "./ref.directive";
 
 
 @Component({
@@ -7,5 +9,20 @@ import {Component, OnInit} from '@angular/core'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  modal = false
+
+  @ViewChild(RefDirective, {static: false}) refDir: RefDirective
+
+  constructor(public resolver: ComponentFactoryResolver) {}
+
+  showModal() {
+    const modalFactory = this.resolver.resolveComponentFactory(ModalComponent)
+    this.refDir.containerRef.clear()
+
+    const component = this.refDir.containerRef.createComponent(modalFactory)
+
+    component.instance.title = 'Dynamic title'
+    component.instance.close.subscribe(() => {
+      this.refDir.containerRef.clear()
+    })
+  }
 }
