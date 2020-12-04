50c62b9aa8e17329328d69e5a613236bef48fdd8
Vlad <v.rozanoff@gmail.com>
2020-12-05 00:06:47 +0200

    Set title and add meta tags in head section of DOM

--- testProj/src/app/app.component.ts
+++ testProj/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, ComponentFactoryResolver, OnInit, ViewChild} from '@angular/core'
-import {ModalComponent} from "./modal/modal.component";
-import {RefDirective} from "./ref.directive";
+import {ModalComponent} from './modal/modal.component'
+import {RefDirective} from './ref.directive'
+import {Meta, Title} from '@angular/platform-browser'
 
 
 @Component({
@@ -12,7 +13,17 @@ export class AppComponent {
 
   @ViewChild(RefDirective, {static: false}) refDir: RefDirective
 
-  constructor(public resolver: ComponentFactoryResolver) {}
+  constructor(
+    public resolver: ComponentFactoryResolver,
+    public title: Title,
+    public meta: Meta
+  ) {
+    this.title.setTitle('oppo')
+    this.meta.addTags([
+      {name: 'keywords', content: 'trump, deep state, fakenews'},
+      {name: 'description', content: 'fuck traditional medicine'}
+    ])
+  }
 
   showModal() {
     const modalFactory = this.resolver.resolveComponentFactory(ModalComponent)
