574559915925718cb7788bb9df1a9b9831f9bf08
Vlad <v.rozanoff@gmail.com>
2020-12-08 20:51:03 +0200

    Add beforeEach statement before tests

--- testProj/src/app/counter/counter.component.spec.ts
+++ testProj/src/app/counter/counter.component.spec.ts
@@ -1,15 +1,18 @@
 import {CounterComponent} from './counter.component'
 
 describe('CounterComponent', () => {
+  let component: CounterComponent
+
+  beforeEach(() => {
+     component = new CounterComponent()
+  })
 
   it('should increment counter by 1', () => {
-    const component = new CounterComponent()
     component.increment()
     expect(component.counter).toBe(1)
   })
 
   it('should decrement counter by 1', () => {
-    const component = new CounterComponent()
     component.decrement()
     expect(component.counter).toBe(-1)
   })
