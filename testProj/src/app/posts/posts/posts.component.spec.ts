2cca49346b926bfab06ff49b0595a84b4f30db42
Vlad <v.rozanoff@gmail.com>
2020-12-17 22:28:04 +0200

    Test adding and updating posts

--- testProj/src/app/posts/posts/posts.component.spec.ts
+++ testProj/src/app/posts/posts/posts.component.spec.ts
@@ -1,7 +1,32 @@
+import {PostsComponent} from "./posts.component";
+import {PostsService} from "./posts.service";
+import {EMPTY, of} from "rxjs";
+
 describe('PostsComponent', () => {
+  let component: PostsComponent
+  let service: PostsService
 
-  it('', () => {
+  beforeEach( () => {
+    service = new PostsService(null)
+    component = new PostsComponent(service)
+  })
 
+
+  it('should update posts length after Ng On Init', () => {
+    const spy = spyOn(service, 'fetch').and.callFake( () => {
+      return of([1,2,3])
+    })
+
+    expect(component.posts.length).toBe(3)
+  })
+
+  it('should add new post', () => {
+    const spy = spyOn(service, 'create').and.returnValue(EMPTY)
+
+    component.add('test')
+
+    expect(spy).toHaveBeenCalled()
+    expect(component.posts.length).toBe(1)
   })
 
 })
